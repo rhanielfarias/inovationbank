1f8905dc1b01a7be08bc256219e5bb0209c07d63
rhanielfarias <rhanielfarias@gmail.com>
2025-03-12 14:45:45 -0300

    Estrutura do spring startando na versão 3 com jdk17

---
+++ src/main/java/inovationbank/inovationbank/InovationbankApplication.java
@@ -0,0 +1,13 @@
+package inovationbank.inovationbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovationbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovationbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inovationbank/inovationbank/InovationbankApplicationTests.java
@@ -0,0 +1,13 @@
+package inovationbank.inovationbank;
+
+import org.junit.jupiter.api					.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InovationbankApplicationTests {
+
+@Test
+	void contextLoads() {
+	}
+
+}
